b6dbe51b873546973f505914ee17f7eb36d86e03
catdoch <cemdocherty@gmail.com>
2017-09-19 19:34:31 +0100

    chore(*): initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Movie DB
+
+- npm run webpack
+- npm run start
+- go to 127.0.0.1
---
+++ js/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {};
+    }
+
+
+    /**
+     * Render giftlist
+     */
+    render() {
+        return (
+            <div>
+                Movie DB
+            </div>
+        );
+    }
+}
