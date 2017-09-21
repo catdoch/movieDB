af956a707e62f395a74a0f9d6dd51878161e87cb
catdoch <cemdocherty@gmail.com>
2017-09-21 08:55:47 +0100

    feat(*): add in styling and search on enter

--- js/components/Home.js
+++ js/components/Home.js
@@ -9,13 +9,16 @@ export default class Home extends Component {
         this.state = {
             data: [],
             searchValue: '',
-            searchInput: ''
+            searchInput: '',
+            showEmpty: false
         };
 
         this.renderAPI = this.renderAPI.bind(this);
         this.handleOnChange = this.handleOnChange.bind(this);
         this.getAPI = this.getAPI.bind(this);
-        this.search = this.search.bind(this);
+        this.handleEnterSubmit = this.handleEnterSubmit.bind(this);
+        this.showEmpty = this.showEmpty.bind(this);
+        this.filterByPopularity = this.filterByPopularity.bind(this);
     }
 
     getAPI() {
@@ -23,16 +26,14 @@ export default class Home extends Component {
 
         api.getSearchAPI(searchValue)
         .then((data) => {
-            this.setState({
-                data: data.results
-            });
+            if (data.total_results > 0) {
+                this.setState({
+                    data: data.results
+                });
+            } else {
+                this.setState({ showEmpty: true });
+            }
         });
-    }
-
-    renderAPI() {
-        const { data } = this.state;
-
-        return data.map(searchData => <ResultsBlock key={searchData.id} data={searchData} />);
     }
 
 
@@ -44,8 +45,38 @@ export default class Home extends Component {
         });
     }
 
-    search() {
-        this.getAPI();
+    handleEnterSubmit(event) {
+        if (event.which === 13 || event.keyCode === 13) {
+            event.preventDefault();
+            this.getAPI();
+            return false;
+        }
+        return true;
+    }
+
+    renderAPI() {
+        const { data } = this.state;
+
+        return data.map(searchData => <ResultsBlock key={searchData.id} data={searchData} />);
+    }
+
+    showEmpty() {
+        const { searchValue, showEmpty } = this.state;
+
+        if (showEmpty) {
+            return <p>{`No results found for ${searchValue}`}</p>;
+        }
+    }
+
+    filterByPopularity() {
+        const { data } = this.state;
+        let sortedArray = [];
+
+        sortedArray = Object.keys(data).reduce((a, b) => {
+            return data.vote_average[a] > data.vote_average[b] ? a : b
+        });
+
+        console.log(sortedArray);
     }
 
 
@@ -55,9 +86,18 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <input type="text" name="searchValue" value={this.state.searchValue} onChange={this.handleOnChange} />
-                <button onClick={this.search}>Search</button>
-                { this.renderAPI() }
+                <h1 className="c-callToAction">Search for a film below</h1>
+                <div className="c-header">
+                    <input className="c-searchInput" type="text" onKeyDown={this.handleEnterSubmit} name="searchValue" value={this.state.searchValue} onChange={this.handleOnChange} />
+                    <button className="c-searchInput__button" onClick={this.getAPI}>Search</button>
+                </div>
+                <div className="c-filterBlock">
+                    <button className="c-filterBlock_button" onClick={this.filterByPopularity}>Popularity</button>
+                </div>
+                <div className="c-resultsBlock__container">
+                    { this.renderAPI() }
+                    { this.showEmpty() }
+                </div>
             </div>
         );
     }
--- js/components/ResultsBlock.js
+++ js/components/ResultsBlock.js
@@ -7,8 +7,8 @@ import PropTypes from 'prop-types';
  */
 const ResultsBlock = ({ data }) => (
     <div className="c-resultsBlock__card">
-        <p>{ data.title }</p>
         <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${data.backdrop_path}`} />
+        <p>{ data.title }</p>
     </div>
 );
 
